1952590bd01062c1c38520ab8a55efde1268a6d3
Nathaniel Alcala <semblancewarden@Gmail.com>
2023-04-21 16:45:32 +0800

    Created ability handler

---
+++ Assets/Scripts/AbilityHandler.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum AbilityType
+{
+    Passive,
+    Active
+}
+
+public class Ability
+{
+    public AbilityHandler abilityHandler;
+    
+    protected string name;
+    protected string description;
+    protected string loreDescription;
+    protected AbilityType type = AbilityType.Passive;
+    
+    public string Name => name;
+    public string Description => description;
+    public string LoreDescription => loreDescription;
+    public AbilityType Type => type;
+
+    public virtual void OnAdded()
+    {
+        //Code triggers when the ability is first added to the entity
+    }
+
+    public virtual void Update()
+    {
+        //Code triggers on Update() from the handler
+    }
+
+    public virtual void FixedUpdate()
+    {
+        //Code triggers on FixedUpdate from the handler
+    }
+}
+
+public class AbilityHandler : MonoBehaviour
+{
+    [SerializeField] private List<Ability> abilities;
+
+    public List<Ability> Abilities => abilities;
+
+    private void Update()
+    {
+        for (int i = 0; i < abilities.Count; i++)
+        {
+            abilities[i].Update();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        for (int i = 0; i < abilities.Count; i++)
+        {
+            abilities[i].FixedUpdate();
+        }
+    }
+
+    public void AddAbility(Ability ability)
+    {
+        abilities.Add(ability);
+        ability.abilityHandler = this;
+        ability.OnAdded();
+    }
+}
--- Assets/Scripts/Tower/UpgradeHandlers/RiflerUpgradeHandler.cs
+++ Assets/Scripts/Tower/UpgradeHandlers/RiflerUpgradeHandler.cs
@@ -9,7 +9,7 @@ public class RiflerUpgradeHandler : TowerUpgradeHandler
     {
         UpgradeGroup level1 = new UpgradeGroup();
         level1.upgradeProperties.Add(new UpgradeProperty(reloadTime: -1));
-        level1.upgradeProperties.Add(new UpgradeProperty(damage: 5));
+        level1.upgradeProperties.Add(new UpgradeProperty(damage: 1));
         upgradeGroups.Add(level1);
 
         UpgradeGroup level2 = new UpgradeGroup();

7193acb3c75dd9dc0d21216df778849fa04306ae
Nathaniel Alcala <semblancewarden@Gmail.com>
2023-08-02 13:28:58 +0800

    Minor fix to loading the correct scene on play

--- Assets/Scripts/UI/MainScreenUI.cs
+++ Assets/Scripts/UI/MainScreenUI.cs
@@ -32,7 +32,7 @@ public class MainScreenUI : MonoBehaviour
 
     private void Start()
     {
-        playBtn.onClick.AddListener(() => SceneController.instance.LoadScene(SceneIndexes.TITLE, MapIndexes.RUINS));
+        playBtn.onClick.AddListener(() => SceneController.instance.LoadScene(SceneIndexes.LEVEL1, MapIndexes.RUINS));
         quitBtn.onClick.AddListener(() => SceneController.instance.QuitGame());
     }
 
